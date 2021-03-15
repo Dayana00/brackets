2e99e392d56e9f99d27b9f4a340d79a872559e9e
Dayana00 <70838027+Dayana00@users.noreply.github.com>
2021-03-16 00:03:38 +0300

    fix: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+    module.exports = function check(str, bracketsConfig) {
+    for (let index = 0; index < bracketsConfig.length; index++) {
+        if(!checPairNumbers(str,bracketsConfig[index][0],bracketsConfig[index][1])){
+            return false;
+        }
+            return checkOpeningClosingBrackets(str,bracketsConfig[index][0],bracketsConfig[index][1])
+        }
+    }
+
+function checkOpeningClosingBrackets(str, firstBracket,secondBracket){
+    let indexFirstBracket=-1;
+    let indexSecondBracket=-1;
+
+    while(true){
+        indexFirstBracket=str.indexOf(firstBracket,indexFirstBracket+1)   ;
+
+        if(firstBracket===secondBracket){
+            secondBracket=firstBracket+1;
+        }
+        indexSecondBracket=str.indexOf(secondBracket,indexSecondBracket+1);
+
+        if(indexFirstBracket===-1||indexSecondBracket===-1){
+            return true
+        }
+
+        if(indexFirstBracket>indexSecondBracket){
+            return false;
+        }else if((indexFirstBracket-indexSecondBracket)%2===0){
+            return false;
+        }
+    }
+}
+
+function checPairNumbers(str,firstBracket,secondBracket){
+    let numberFirstBrackets=str.split(firstBracket).length-1;
+    let numberSecondBrackets=str.split(secondBracket).length-1;
+
+    return numberFirstBrackets===numberSecondBrackets
 }
